4e1d2eab11c4311ac0c8e4f368b6ef556dc97766
Yotakey <keyisakris@gmail.com>
2022-05-25 22:43:29 +0700

    add List and Button

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ root.title("ToDoList")
 SizeWindow = "400x500"
 
 root.geometry(SizeWindow)
+root.resizable(False,False)
 root.iconbitmap("todolisticon.ico")
 
 root.configure(
@@ -18,9 +19,6 @@ root.configure(
 
 #Label Judul
 JudulFrame = Frame(root)
-JudulFrame.configure(
-
-)
 JudulFrame.pack()
 
 TextJudul = StringVar()
@@ -40,27 +38,47 @@ Judul.configure(
 Judul.pack()
 
 Input = Frame(root)
+
 #Entry Text Box
 InputToDo = Entry(Input,
-    relief = "flat",
+    relief = "ridge",
     bd = 5,
     bg= "#c8d4e8",
     width = 50,
     justify= "center",
-    borderwidth=10
+    borderwidth=10,
 )
 
 InputToDo.pack()
-Input.pack(pady=20)
+Input.pack(pady=10)
+
+#Button
+BtnFrame = Frame(root)
+
+def add_task():
+    pass
+def delete_task():
+    pass
+def done_task():
+    pass
+
+add = Button(BtnFrame,text = "Add",command=add_task)
+delete = Button(BtnFrame,text = "Delete",command=delete_task)
+done = Button(BtnFrame,text = "Done",command=done_task)
+
+add.grid(row = 0, column = 0, padx = 15)
+delete.grid(row = 0, column = 1, padx = 15)
+done.grid(row = 0, column = 2, padx = 15)
+
+BtnFrame.configure(bg = "#ffffff")
+BtnFrame.pack(pady = 10)
 
 
 #to do list
 
 ListFrame = Frame(root)
 
-data = [
-    "satu","dua","tiga", "empat"
-    ]
+data = [x for x in range(30)]
 
 todo = Listbox(ListFrame)
 for do in data:
@@ -68,7 +86,12 @@ for do in data:
 todo.configure(
     width = 50
 )
-todo.pack()
+scroll = Scrollbar(ListFrame)
+
+todo.configure(yscrollcommand=scroll.set)
+todo.pack(side=LEFT, fill=BOTH)
+scroll.configure(command = todo.yview)
+scroll.pack(side=RIGHT,fill = BOTH)
 ListFrame.pack()
 
 root.mainloop()
